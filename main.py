afa5702ff76c4138057604d32852ae53e236d209
rayyan77pra-dev <rayyan77pra@gmail.com>
2026-06-07 16:26:37 -0400

    main.py

--- main.py
+++ main.py
@@ -44,11 +44,6 @@ async def handler(websocket):
                     broadcast_packet = {"cmd": "gvar", "name": data.get("name"), "val": data.get("val")}
                     websockets.broadcast(connected_clients, json.dumps(broadcast_packet))
 
-                # Test Code For Running
-                elif cmd == "test":
-                    broadcast_packet = {"cmd": "test", "name": data.get("name"), "val": data.get(int("val") + 1)}
-                    websockets.broadcast(connected_clients, json.dumps(broadcast_packet))
-
             except json.JSONDecodeError:
                 # Catch bad JSON formatting cleanly without dropping the server offline
                 pass
